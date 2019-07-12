f98346b27b40f593dbbc64eea9d973b688de60e3
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-07-11 22:00:12 -0400

    added https

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,8 @@
 import errorHandler from "errorhandler";
 
 import app from "./app";
+import https from "https";
+import fs from "fs";
 
 /**
  * Error Handler. Provides full stack - remove for production
@@ -10,13 +12,25 @@ app.use(errorHandler());
 /**
  * Start Express server.
  */
-const server = app.listen(app.get("port"), () => {
-  console.log(
-    "  App is running at http://localhost:%d in %s mode",
-    app.get("port"),
-    app.get("env")
-  );
-  console.log("  Press CTRL-C to stop\n");
-});
 
+const server = process.env.NODE_ENV == "production" ? https.createServer({
+  key: fs.readFileSync(process.env.KEY),
+  cert: fs.readFileSync(process.env.CERT),
+  passphrase: process.env.PASSPHRASE
+}, app)
+  .listen(app.get("port"), () => {
+    console.log(
+      "  App is running at http://localhost:%d in %s mode",
+      app.get("port"),
+      app.get("env")
+    );
+    console.log("  Press CTRL-C to stop\n");
+  }) : app.listen(app.get("port"), () => {
+    console.log(
+      "  App is running at http://localhost:%d in %s mode",
+      app.get("port"),
+      app.get("env")
+    );
+    console.log("  Press CTRL-C to stop\n");
+  });
 export default server;
